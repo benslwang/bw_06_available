8d2b3c6cb3ce642449bbe460011abace0fba9f64
benslwang <363111516@qq.com>
2022-06-20 21:32:49 +0800

    面向对象

--- bw_10_面向对象基础语法.py
+++ bw_10_面向对象基础语法.py
@@ -31,14 +31,19 @@ class 类名:
 class Cat:
 
     def eat(self):
-        print("小猫爱吃鱼")
+        # 哪一个对象调用的方法，self就是哪一个对象的引用
+        print("%s爱吃鱼" % self.name)
 
     def drink(self):
-        print("小猫要喝水")
+        print("%s要喝水" % self.name)
 
 
 # 创建猫对象
 tom = Cat()
+
+# 增加属性（可以使用 .属性名 利用赋值语句）
+tom.name = "Tom"
+
 
 tom.eat()
 tom.drink()
@@ -54,6 +59,9 @@ print("%x" % addr)
 
 # 再创建一个猫对象
 lazy_cat = Cat()
+
+# 增加属性
+lazy_cat.name = "大懒猫"
 
 lazy_cat.eat()
 lazy_cat.drink()
