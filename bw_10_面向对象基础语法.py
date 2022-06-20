af1eb1367129bd93015c4b72af3727ae4a991232
benslwang <363111516@qq.com>
2022-06-20 21:10:40 +0800

    面向对象

--- bw_10_面向对象基础语法.py
+++ bw_10_面向对象基础语法.py
@@ -2,6 +2,8 @@
 dir 内置函数
     使用内置函数dir传入标识符/数据，可以查看对象内的所有属性与方法
 """
+
+
 def demo():
     """这是一个测试函数"""
     print("hello python")
@@ -24,3 +26,36 @@ class 类名:
     当一个类定义完成后，要使用这类来创建对象，语法格式如下：
 变量对象 = 类名()
 """
+
+
+class Cat:
+
+    def eat(self):
+        print("小猫爱吃鱼")
+
+    def drink(self):
+        print("小猫要喝水")
+
+
+# 创建猫对象
+tom = Cat()
+
+tom.eat()
+tom.drink()
+
+print(tom)
+
+# 内存地址
+addr = id(tom)
+# 十进制
+print("%d" % addr)
+# 十六进制
+print("%x" % addr)
+
+# 再创建一个猫对象
+lazy_cat = Cat()
+
+lazy_cat.eat()
+lazy_cat.drink()
+
+print(lazy_cat)
